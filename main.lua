b5aa226df65ce3559ef40ed9b73635f2f0c17a05
cash <99702435+cashmerepancake@users.noreply.github.com>
2023-01-25 22:19:02 -0700

    added draw delay. and dt to update.
     dont know how i missed that i didnt have delta time lmaooo. how did that work

--- main.lua
+++ main.lua
@@ -2,13 +2,13 @@
 
 local home=require'screens.home' --load home screen
 local menu=require'screens.menu' --load menus (may end up not having any menus? idk how im gonna do this tbh)
+--main argument for screens is for when i (fingers crossed) add more things to gamble on
 
 function love.load()
     --###INIT BIG IMPORTANT VARAIBELS###--
     world={} --what goes to screen, calls its contents update/draw, add or delete screen tables to switch scenes
     h=home.screen --home screen, written in home.lua, holds all data (update/draw) for this scene
     e=menu.exit --exit menu
-    currentScreen="home" --for screen changes(?probably will change and handle it a different, better way if i end up needing it at all)
 
     --###add to tables###--
     table.insert(world,1,h) --begin on home screen. specify index as 1 as thats the index we'll call
@@ -16,8 +16,10 @@ function love.load()
     world[1]:load()
 end
 
-function love.update()
+function love.update(dt)
+    --love.timer.sleep(1/60)
     world[1]:update()end
 
 function love.draw()
+    love.timer.sleep(1/60)
     world[1]:draw()end
