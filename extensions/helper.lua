111c46fa7cd2ce3088de0751ee85f89358304656
cash <99702435+cashmerepancake@users.noreply.github.com>
2023-01-25 22:16:33 -0700

    more stuffs in helper

--- extensions/helper.lua
+++ extensions/helper.lua
@@ -19,4 +19,20 @@ function helper.hasIndex(table,val)
     end
 end
 
+--rounds positive number to nearest postitive int (TODO: account for negative lmao)
+function helper.round(n)
+    if(n-math.floor(n)>0.5)then return math.floor(n)+1 
+    else return math.floor(n)end
+end
+
+--random float :0 
+function helper.randomFloat(min, max, precision)
+	local precision = precision or 0
+	local num = math.random()
+	local range = math.abs(max - min)
+	local offset = range * num
+	local randomnum = min + offset
+	return math.floor(randomnum * math.pow(10, precision) + 0.5) / math.pow(10, precision)
+end
+
 return helper
